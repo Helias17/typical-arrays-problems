e8d4724aa58f78609a3a9d81825571511e6dd2bd
Ilya Komar <helias@yandex.ru>
2020-09-30 15:52:21 +0300

    feat: add functions for searching min, max and average values of array

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
 
 exports.min = function min (array) {
+  if ( !array || array.length === 0) return 0;
 
-  return 0;
+  array.sort( (a,b) => {
+    return a-b;
+  } )
+
+  return array[0];
 }
 
 exports.max = function max (array) {
+  if ( !array || array.length === 0) return 0;
 
-  return 0;
+  array.sort( (a,b) => {
+    return a-b;
+  } )
+
+  return array.reverse()[0];
 }
 
 exports.avg = function avg (array) {
 
-  return 0;
+  if ( !array || array.length === 0 ) return 0;
+
+  let val = array.reduce( (prev, el) => {
+    return prev += el;
+  }, 0 )
+
+  return val/array.length;
 
 }
